b74976e6e53959ecf4ca530b88dee7c6b5699eed
Satyajit Gokhale <twisterintwist@gmail.com>
2024-06-05 12:19:58 -0400

    after transformation

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
+import { memo } from "react";
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import Search from './components/Search';
 import Results from './components/Results';
 import './App.css';
-
-function App() {
-  const [source, setSource] = useState('StackOverflow')
-  const [query, setQuery] = useState('')
-
-  return (
-    <div className="App">
+const App = memo(function App() {
+  const [source, setSource] = useState('StackOverflow');
+  const [query, setQuery] = useState('');
+  return <div className="App">
       <Search source={source} setSource={setSource} query={query} setQuery={setQuery} />
       <Results source={source} query={query} />
-    </div>
-  );
-}
-
+    </div>;
+});
 export default App;
