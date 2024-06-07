a1e1dcb8864979c9cc3549521d7578a37413dc6b
Satyajit Gokhale <twisterintwist@gmail.com>
2024-06-07 13:24:20 -0400

    after transformation

--- src/components/None.tsx
+++ src/components/None.tsx
@@ -1,15 +1,11 @@
+import { memo } from "react";
 import React from 'react';
-import Nothing from "../assets/nothing.jpeg"
-import "../styles/None.css"
-
-
-function None() {
-  return (
-    <div className='none'>
+import Nothing from "../assets/nothing.jpeg";
+import "../styles/None.css";
+const None = memo(function None() {
+  return <div className='none'>
         <img className='nothing' src={Nothing} alt="nothing" />
         <h1>No results found :\</h1>
-    </div>
-  );
-}
-
+    </div>;
+});
 export default None;
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,54 +1,42 @@
+import { memo } from "react";
 import React, { useState, useRef } from 'react';
-import "../styles/Search.css"
-
-
+import "../styles/Search.css";
 type SearchType = {
-    query: string,
-    setQuery: React.Dispatch<React.SetStateAction<string>>,
-    source: string,
-    setSource: React.Dispatch<React.SetStateAction<string>>,
-}
-
-
-function Search({ query, setQuery, source, setSource }: SearchType) {
-    const [tempQuery, setTempQuery] = useState('')
-    const searchText = useRef<HTMLInputElement>(null);
-
-    function handleSearchChange(e: React.FormEvent<HTMLFormElement>) {
-        e.preventDefault()
-
-        if (searchText.current != null) {
-            const checkInput = searchText.current.value;
-
-            if (checkInput !== '') {
-                setQuery(tempQuery)
-            } 
-        }
+  query: string;
+  setQuery: React.Dispatch<React.SetStateAction<string>>;
+  source: string;
+  setSource: React.Dispatch<React.SetStateAction<string>>;
+};
+const Search = memo(function Search({
+  query,
+  setQuery,
+  source,
+  setSource
+}: SearchType) {
+  const [tempQuery, setTempQuery] = useState('');
+  const searchText = useRef<HTMLInputElement>(null);
+  function handleSearchChange(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (searchText.current != null) {
+      const checkInput = searchText.current.value;
+      if (checkInput !== '') {
+        setQuery(tempQuery);
+      }
     }
-
-    return (
-        <div className='search'>
+  }
+  return <div className='search'>
             <h1 className='title'>Me.com</h1>
-            <form className='form' onSubmit={(e) => handleSearchChange(e)}>
-                <input
-                    type="search"
-                    placeholder="Start typing to search..."
-                    onChange={(e) => {setTempQuery(e.target.value)}}
-                    value={tempQuery} 
-                    ref={searchText}
-                    className='search-box' />
-                <select 
-                defaultValue={source}
-                onChange={(e) => setSource(e.target.value)}
-                className='dropdown'>
+            <form className='form' onSubmit={e => handleSearchChange(e)}>
+                <input type="search" placeholder="Start typing to search..." onChange={e => {
+        setTempQuery(e.target.value);
+      }} value={tempQuery} ref={searchText} className='search-box' />
+                <select defaultValue={source} onChange={e => setSource(e.target.value)} className='dropdown'>
                     <option value="StackOverflow">StackOverflow</option>
                     <option value="GIPHY">GIPHY</option>
                     <option value="Wikipedia">Wikipedia</option>
                 </select>
                 <button className='search-btn' type="submit">Search</button>
             </form>
-        </div>
-    );
-}
-  
+        </div>;
+});
 export default Search;
